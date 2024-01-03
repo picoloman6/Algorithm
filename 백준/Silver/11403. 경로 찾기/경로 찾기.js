ee9b4271d1070d789f49d3bba4b8d94758c06bca
picoloman6 <85938399+picoloman6@users.noreply.github.com>
2024-01-03 11:20:04 +0900

    [Silver I] Title: 경로 찾기, Time: 220 ms, Memory: 10424 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.js
@@ -0,0 +1,36 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on("line", (line) => {
+  input.push(line);
+}).on("close", () => {
+  solution(input);
+});
+
+const solution = (input) => {
+  const dist = [];
+
+  for (let i = 1; i < input.length; i++) {
+    dist.push(input[i].split(" ").map(Number));
+  }
+
+  for (let k = 0; k < dist.length; k++) {
+    for (let i = 0; i < dist.length; i++) {
+      for (let j = 0; j < dist.length; j++) {
+        if (dist[i][k] === 1 && dist[k][j] === 1) {
+          dist[i][j] = 1;
+        }
+      }
+    }
+  }
+
+  for (let i = 0; i < dist.length; i++) {
+    console.log(dist[i].join(" "));
+  }
+};
