3ab415c8d4fad789507f515f9085206e34fbb8f2
picoloman6 <85938399+picoloman6@users.noreply.github.com>
2024-01-08 15:45:42 +0900

    [level 2] Title: 숫자 변환하기, Time: 96.27 ms, Memory: 50 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -1,23 +1,54 @@
 function solution(x, y, n) {
-    const arr = Array.from({ length : y + 1 }, () => Infinity);
+    const dp = Array.from({ length : y + 1 }, () => Infinity);
     
     for(let i = 0; i <= x; i++) {
-        arr[i] = 0;
+        dp[i] = 0;
     }
     
     for(let i = x; i < y; i++) {
         if(i + n <= y) {
-            arr[i + n] = Math.min(arr[i] + 1, arr[i + n]);
+            dp[i + n] = Math.min(dp[i] + 1, dp[i + n]);
         }
         
         if(i * 2 <= y) {
-            arr[i * 2] = Math.min(arr[i] + 1, arr[i * 2]);
+            dp[i * 2] = Math.min(dp[i] + 1, dp[i * 2]);
         }
         
         if(i * 3 <= y) {
-            arr[i * 3] = Math.min(arr[i] + 1, arr[i * 3]);
+            dp[i * 3] = Math.min(dp[i] + 1, dp[i * 3]);
         }
+        
     }
-    
-    return arr[y] === Infinity ? -1 : arr[y];
+            
+    return dp[y] === Infinity ? -1 : dp[y];
 }
+
+// function solution(x, y, n) {
+//     const arr = Array.from({ length : y + 1 }, () => Infinity);
+    
+//     for(let i = 0; i <= x; i++) {
+//         arr[i] = 0;
+//     }
+    
+//     for(let i = x; i < y; i++) {
+//         if(i + n <= y) {
+//             arr[i + n] = Math.min(arr[i] + 1, arr[i + n]);
+//         }
+//     }
+    
+//     for(let i = x; i < y; i++) {
+//         if(i * 2 <= y) {
+//             arr[i * 2] = Math.min(arr[i] + 1, arr[i * 2]);
+//         }
+//     }
+    
+//     for(let i = x; i < y; i++) {
+//         if(i * 3 <= y) {
+//             arr[i * 3] = Math.min(arr[i] + 1, arr[i * 3]);
+//         }
+//     }
+    
+//     console.log(arr);
+        
+//     return arr[y] === Infinity ? -1 : arr[y];
+// }
