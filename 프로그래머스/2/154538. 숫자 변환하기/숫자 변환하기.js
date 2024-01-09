488cf0d153b1351fb6fc7cc9817d9460dbaa0cf4
picoloman6 <85938399+picoloman6@users.noreply.github.com>
2024-01-09 09:54:18 +0900

    [level 2] Title: 숫자 변환하기, Time: 81.01 ms, Memory: 50.1 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -22,33 +22,3 @@ function solution(x, y, n) {
             
     return dp[y] === Infinity ? -1 : dp[y];
 }
-
-// function solution(x, y, n) {
-//     const arr = Array.from({ length : y + 1 }, () => Infinity);
-    
-//     for(let i = 0; i <= x; i++) {
-//         arr[i] = 0;
-//     }
-    
-//     for(let i = x; i < y; i++) {
-//         if(i + n <= y) {
-//             arr[i + n] = Math.min(arr[i] + 1, arr[i + n]);
-//         }
-//     }
-    
-//     for(let i = x; i < y; i++) {
-//         if(i * 2 <= y) {
-//             arr[i * 2] = Math.min(arr[i] + 1, arr[i * 2]);
-//         }
-//     }
-    
-//     for(let i = x; i < y; i++) {
-//         if(i * 3 <= y) {
-//             arr[i * 3] = Math.min(arr[i] + 1, arr[i * 3]);
-//         }
-//     }
-    
-//     console.log(arr);
-        
-//     return arr[y] === Infinity ? -1 : arr[y];
-// }
