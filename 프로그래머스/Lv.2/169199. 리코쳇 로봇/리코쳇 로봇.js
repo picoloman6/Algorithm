d79dceba6774cef3a77fba45551d49fb196bd3ff
picoloman6 <85938399+picoloman6@users.noreply.github.com>
2024-01-12 09:42:34 +0900

    [level Lv.2] Title: 리코쳇 로봇, Time: 1.65 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/169199. 리코쳇 로봇/리코쳇 로봇.js
@@ -0,0 +1,50 @@
+function bfs(board, start) {
+    const answer = [];
+    const n = board.length;
+    const m = board[0].length;
+    const dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+    const check = Array.from({ length : n }, () => new Array(m).fill(false));
+    const queue = [];
+    
+    queue.push(start);
+    check[start.x][start.y] = true;
+    
+    while(queue.length > 0) {
+        const { x, y, cnt } = queue.shift();
+        
+        if(board[x][y] === 'G') {
+            answer.push(cnt);
+        } else {
+            for(const [dx, dy] of dirs) {
+                let nx = x + dx;
+                let ny = y + dy;
+
+                while(nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] !== 'D') {
+                    nx += dx;
+                    ny += dy;
+                }
+                
+                nx -= dx;
+                ny -= dy;
+                
+                if(!check[nx][ny]) {
+                    queue.push({ x : nx, y : ny, cnt : cnt + 1 });
+                    check[nx][ny] = true;
+                }
+            }
+        }
+    }
+    
+    return answer;
+}
+
+function solution(board) {
+    outer:for(let i = 0; i < board.length; i++) {
+        for(let j = 0; j < board[0].length; j++) {
+            if(board[i][j] === 'R') {
+                const answer = bfs(board, { x : i, y : j, cnt : 0 });
+                return answer.length === 0 ? - 1 : Math.min(...answer);
+            }
+        }
+    }
+}
